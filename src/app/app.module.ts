48637d4cdbde9fcc32c0dc32b6859ace1cb19fe9
jesusbarcam <jesusbarcam@gmail.com>
2018-07-18 21:09:33 +0200

    Created navigation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
 import { NgModule, RootRenderer } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { UnknowPageComponent } from './components/unknow-page/unknow-page.component';
+import { HeaderComponent } from './components/header/header.component';
 import { PrivateZoneModule } from './modules/private-zone/private-zone.module';
 import { LandingZoneModule } from './modules/landing-zone/landing-zone.module';
 import { RoutingModule } from './routing.module';
 
 
-
 @NgModule({
   imports: [
     BrowserModule,
+    RouterModule,
     RoutingModule,
     CommonModule,
     PrivateZoneModule,
@@ -20,7 +23,8 @@ import { RoutingModule } from './routing.module';
   ],
   declarations: [
     AppComponent,
-    UnknowPageComponent
+    UnknowPageComponent,
+    HeaderComponent
   ],
   providers: [],
   bootstrap: [ AppComponent ]
