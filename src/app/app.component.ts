22e037f0fb8e6627842bd4acd30492b85d227197
jesusbarcam <jesusbarcam@gmail.com>
2018-07-18 23:34:02 +0200

    Installed nge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+
+  constructor(private translateService: TranslateService) {
+    // Default translate language value
+    // This language is used by default from the init
+    translateService.setDefaultLang('es');
+  }// Constructor
+
 }// AppComponent
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, RootRenderer } from '@angular/core';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
@@ -11,12 +14,25 @@ import { PrivateZoneModule } from './modules/private-zone/private-zone.module';
 import { LandingZoneModule } from './modules/landing-zone/landing-zone.module';
 import { RoutingModule } from './routing.module';
 
+// This function make of constructor to Loader inside of Translate Module,
+// We send this function when called to TranslateModule forRoot function.
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}// HttpLoaderFactory
 
 @NgModule({
   imports: [
     BrowserModule,
     RouterModule,
     RoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot( {
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [ HttpClient ]
+      }
+    }),
     CommonModule,
     PrivateZoneModule,
     LandingZoneModule
