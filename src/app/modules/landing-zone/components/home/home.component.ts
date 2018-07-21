4cac0aec60879bf19a5eca040bbc829292501332
jesusbarcam <jesusbarcam@gmail.com>
2018-07-21 09:30:08 +0200

    Implemented home section template and add landingZoneModuleSettings service

--- src/app/modules/landing-zone/components/home/home.component.ts
+++ src/app/modules/landing-zone/components/home/home.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { LandingZoneModuleSettings } from '../../services/settings/landing-zone-module-settings.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  private _designHomeImageURL;
+
+
+  constructor() {
+  }// Constructor
+
+
 
   ngOnInit() {
-  }
+    this._designHomeImageURL = LandingZoneModuleSettings.HOME_1_SECTION_URL;
+  }// NgOnInit
 
-}
+
+  public get designHomeImageURL() {
+    return this._designHomeImageURL;
+  }// DesignHomeImageURL
+
+}// HomeComponent
---
+++ src/app/modules/landing-zone/services/settings/landing-zone-module-settings.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+
+@Injectable()
+export class LandingZoneModuleSettings {
+
+  public static readonly HOME_WALLPAPER_URL: string = 'assets/images/landingPage_wallpaper_filter1.jpg';
+  public static readonly ABOUT_US_WALLPAPER_URL: string = 'assets/images/aboutus_image.jpg';
+  public static readonly HOW_WE_WORK_WALLPAPER_URL: string = 'assets/images/howwework_image.jpg';
+  public static readonly GALLERY_WALLPAPER_URL: string = 'assets/images/gallery_wallpaper.jpg';
+  public static readonly CONTACT_WALLPAPER_URL: string = 'assets/images/contact_wallpaper.jpg';
+  public static readonly HOME_1_SECTION_URL: string = 'assets/images/home_image_compressed.jpg';
+  public static readonly COMPANY_IMAGE_LOGO_URL: string = 'assets/images/Master_Logo_big.svg';
+
+}// LandingZoneModuleSettings
