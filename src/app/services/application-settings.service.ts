8081818e5ec2cc78b7d644d35336b4efa10c5e81
jesusbarcam <jesusbarcam@gmail.com>
2018-08-23 16:45:50 +0200

    Implemented changes of languages in application

--- src/app/services/application-settings.service.ts
+++ src/app/services/application-settings.service.ts
@@ -8,4 +8,7 @@ export class ApplicationSettingsService {
   public static readonly BIOH_LOGO_URL: string = '../../assets/images/Master_Logo_big.svg';
   public static readonly APPLICATION_VERSION: string = '1.0.0';
 
+  public static readonly INITIAL_LANGUAGE: string = 'es';
+  public static readonly LANGUAGES_LIST: string[] = [ 'es', 'en', 'ru' ];
+
 }// ApplicationSettingsService
